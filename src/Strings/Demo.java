e372e02ad575fdae7ede084a48eace599f967c91
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-03 19:35:42 +0530

    Added programs

---
+++ src/Strings/Demo.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		String s1 = "Java";
+		String s2 = "java";
+		String s3 = s1 + "Developer";
+		String s4 = new String ("JAVA");
+		String s5 = new String ("Java");
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		System.out.println(s4);
+		System.out.println(s5);
+	}
+
+}
