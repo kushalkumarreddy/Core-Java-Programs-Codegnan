0f8f8a32610bd1b1a3eac0ae4940f3d5d2bdbf4c
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-04 18:34:09 +0530

    Added String programs

---
+++ src/Strings/WordCount.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class WordCount {
+	public static void main(String[] args) {
+
+		String s = "I am a java developer";
+
+		String words[] = s.split(" ");
+
+		System.out.println("Number of words: " + words.length);
+	}
+
+}
