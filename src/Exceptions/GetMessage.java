0e52d2a1fe9443d7ab88560e945d89e4391060a9
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-06 09:40:53 +0530

    Added programs

--- src/Exceptions/GetMessage.java
+++ src/Exceptions/GetMessage.java
@@ -1,13 +1,20 @@
 package Exceptions;
 
-public class ExceptionDemo {
+public class GetMessage {
 	public static void main(String[] args) {
+		
 		int num = 10, div = 0,res = 0;
 		
+		try {
 		res = num/div;
+		} catch (Exception e) {
+			e.getMessage();
+			e.printStackTrace();
+		}
 		
 		System.out.println(res);
 		System.out.println("Bye");
+		
 	}
 
 }
