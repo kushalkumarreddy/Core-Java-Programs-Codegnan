7ec0c81485690d6f1d96b7bd1c04825766b81e7b
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-06 09:31:59 +0530

    Added programs

---
+++ src/Exceptions/ExceptionDemo.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+public class ExceptionDemo {
+	public static void main(String[] args) {
+		int num = 10, div = 0,res = 0;
+		
+		res = num/div;
+		
+		System.out.println(res);
+		System.out.println("Bye");
+	}
+
+}
---
+++ src/Exceptions/TryCatch.java
@@ -0,0 +1,18 @@
+package Exceptions;
+
+public class TryCatch {
+	public static void main(String[] args) {
+		
+		int num = 10, div = 0,res = 0;
+		
+		try {
+		res = num/div;
+		} catch (Exception e) {
+			System.out.println(num + " Not divisible by Zero");
+		}
+		
+		System.out.println(res);
+		System.out.println("Bye");
+	}
+
+}
