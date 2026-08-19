e36cedb8abc54c441f80287abd883c28a009eccf
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-19 10:13:37 +0530

    Added program

---
+++ src/map/HashMapDemo.java
@@ -0,0 +1,19 @@
+package map;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+	public static void main(String[] args) {
+		
+		HashMap<Integer, String> hmap = new HashMap<>();
+		
+		hmap.put(101, "rakesh");
+		hmap.put(102, "ramesh");
+		hmap.put(103, "ganesh");
+		hmap.put(102, "suresh"); //replaces old value
+		System.out.println(hmap);
+		
+		
+	}
+
+}
