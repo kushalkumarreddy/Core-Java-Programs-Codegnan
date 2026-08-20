c422823561dfaaffafb750cc73e29a8471c79e26
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-20 10:05:31 +0530

    Added program

--- src/map/WordCount.java
+++ src/map/WordCount.java
@@ -1,5 +1,26 @@
 package map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordCount {
+	public static void main(String[] args) {
+		String s = "hello world hello java world";
+		String[] words = s.split(" ");
+		
+		Map<String, Integer> wc = new HashMap<>();
+		
+		for(String word: words) {
+			if(wc.containsKey(word)) {
+				wc.put(word, wc.get(word)+1);
+			} else {
+				wc.put(word, 1);
+			}
+		}
+		
+		for (String word : wc.keySet()) {
+            System.out.println(word + " = " + wc.get(word));
+        }
+	}
 
 }
