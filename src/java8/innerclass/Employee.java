51262dec8707461ad23e65e6fd1c6c6a2714dec8
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-24 17:38:11 +0530

    updated

--- src/java8/innerclass/Employee.java
+++ src/java8/innerclass/Employee.java
@@ -1,12 +1,26 @@
 package java8.innerclass;
 
-public class emp {
-	int eid;
-	public void getDetails() {
-		System.out.println("It is employee class");
-	}
+class Emp {
+    int eid;
+    
+    public void getDetails() {
+        System.out.println("It is employee class");
+    }
+
+    class Address {
+        public void getAddress() {
+            System.out.println("It is an address class");
+        }
+    }
 }
 
 public class Employee {
+    public static void main(String[] args) {
 
+        Emp emp = new Emp();
+        emp.getDetails();
+
+        Emp.Address ad = emp.new Address();
+        ad.getAddress();
+    }
 }
