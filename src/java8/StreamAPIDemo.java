168eb41be06ebb707395771f26502ba5e000b26c
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-25 10:21:58 +0530

    updated

--- src/java8/StreamAPIDemo.java
+++ src/java8/StreamAPIDemo.java
@@ -7,8 +7,20 @@ public class StreamAPIDemo {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(10,40,5,30,60,10,40);
 		
-		list.stream()
-		.forEach((n) -> System.out.println(n));
+//		list.stream()
+//		.forEach((n) -> System.out.println(n));
+//		.filter(n -> n>20)
+//		.map(n -> n+30)
+//		.distinct()
+//		.sorted()
+//		.forEach(System.out::println);
+		
+		long count = list.stream()
+				.filter(n -> n>10)
+				.count();
+		
+		System.out.println(count);
+		
 	}
 
 }
