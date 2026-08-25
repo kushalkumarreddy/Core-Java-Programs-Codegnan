31b5371cb2861d2d9514497336bb4e52f64f6fc7
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-25 09:54:19 +0530

    updated

---
+++ src/java8/StreamAPIDemo.java
@@ -0,0 +1,13 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamAPIDemo {
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(10,40,5,30,60,10,40);
+		list.add(20);
+		
+	}
+
+}
