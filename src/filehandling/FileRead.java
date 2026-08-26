60eaa2aaa4bba6aa61cbb7d188e8c1819dfa92e9
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-26 10:24:17 +0530

    updated

--- src/filehandling/FileRead.java
+++ src/filehandling/FileRead.java
@@ -1,10 +1,27 @@
 package filehandling;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileRead {
 	public static void main(String[] args) {
-		FileInputStream fis = new FileInputStream("std.txt");
+		
+		try {
+			FileInputStream fis = new FileInputStream("std.txt");
+			int data;
+			
+			while((data = fis.read())!= -1) {
+				System.out.print((char)data);
+			}
+			fis.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
